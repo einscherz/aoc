049e0968c543a065b12535ac6c93abf6a765704b
einscherz <einscherz@protonmail.com>
2024-12-14 19:01:50 +0100

    settle with better solution for d14

--- y2024/d14/main.py
+++ y2024/d14/main.py
@@ -1,35 +1,20 @@
 import math
 import re
 import sys
+sign=lambda x:(x>0)-(x<0)
+def stdev(a):
+    avg=sum(a)/len(a)
+    return math.sqrt(sum([(x-avg)**2 for x in a])/len(a))
 inp=sys.stdin.read().strip()
 R,C=103,101
 grid=[['.' for j in range(C)] for i in range(R)]
-robots=[]
-for x in inp.split('\n'):
-    robots.append(list(map(int,re.findall(r'-?\d+',x))))
-i=0
-ans1,ans2=0,0
-while not (ans1 and ans2):
-    if i==100:
-        quad=[0]*4
-        for x in robots:
-            quad[2*(x[0]<C//2)+(x[1]<R//2)]+=x[0]!=C//2 and x[1]!=R//2
-        ans1=math.prod(quad)
-    if not ans2:
-        for x in robots:
-            grid[x[1]][x[0]]='#'
-        for x in robots:
-            cntr=1
-            while x[0]+cntr<C and grid[x[1]][x[0]+cntr]=='#':
-                cntr+=1
-            if cntr>8:
-                ans2=i
-                print('\n'.join([''.join(y) for y in grid]))
-                break
-    for j in range(len(robots)):
-        grid[robots[j][1]][robots[j][0]]='.'
-        for k in range(2):
-            robots[j][k]=(robots[j][k]+robots[j][k+2])%([C,R][k])
-    i+=1
+robots=[list(map(int,re.findall(r'-?\d+',x))) for x in inp.split('\n')]
+ans2=(sum([min([(stdev([(x[i]+x[i+2]*j)%[C,R][i] for x in robots]),j) for j in range([C,R][i])])[1]*[R,C][i] for i in range(2)])*51)%(R*C)
+quad=[0]*9
+for x in robots:
+    quad[sign((x[0]+x[2]*100)%C-C//2)+sign((x[1]+x[3]*100)%R-R//2)*3]+=1
+    grid[(x[1]+x[3]*ans2)%R][(x[0]+x[2]*ans2)%C]='#'
+ans1=math.prod([quad[x] for x in [2,4,5,7]])
+print('\n'.join([''.join(x) for x in grid]))
 print(ans1,ans2)
 
